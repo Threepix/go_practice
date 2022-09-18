bc9b759a7eb8a54cd98d448919565b6e82a7b3c8
Threepix <98532056+Threepix@users.noreply.github.com>
2022-09-18 22:19:36 +0300

    Update sorting.go

--- practice_1/sorting.go
+++ practice_1/sorting.go
@@ -3,33 +3,41 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func selectionSort(n int) {
-	posled := make([]int, n)
-	poss := make([]*int, n)
-	for i := 0; i < n; i++ {
+	count := 0
+	now := time.Now()
+
+	posled := make([]int, n) //n
+	poss := make([]*int, n)  //n
+	for i := 0; i < n; i++ { //2n
 		posled[i] = rand.Intn(100)
 		poss[i] = &posled[i]
 	}
-	len_poss := len(poss)
+	len_poss := len(poss) //n
 	fmt.Println("unsorted")
-	for i := 0; i < len_poss; i++ {
+	for i := 0; i < len_poss; i++ { //n
 		fmt.Println(*poss[i])
 	}
-	for i := 0; i < len_poss; i++ {
+	for i := 0; i < len_poss; i++ { // 4n
 		var minInd = i
-		for j := i; j < len_poss; j++ {
+		for j := i; j < len_poss; j++ { //2n
 			if *poss[j] < *poss[minInd] {
 				minInd = j
 			}
 		}
 		poss[i], poss[minInd] = poss[minInd], poss[i]
 	}
-	fmt.Println("sorted")
-	for i := 0; i < len_poss; i++ {
+	fmt.Println("sorted")           //n
+	for i := 0; i < len_poss; i++ { //n
 		fmt.Println(*poss[i])
 	}
+	durat := time.Since(now)
+	fmt.Println(durat) //n
+	count = n + n + n*(2*n) + n + n*n + n*4*n + n + n*n + 3*n
+	fmt.Println(count) //n
 }
 
 func main() {
@@ -37,3 +45,4 @@ func main() {
 	fmt.Scanln(&n)
 	selectionSort(n)
 }
+
